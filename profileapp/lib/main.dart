93c669fd4d55b574983cde5768918094062e57a3
mouse <hyeji111544@gmail.com>
2024-10-01 15:03:31 +0900

    앱 설정 완료

---
+++ profileapp/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:profileapp/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key : key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme(),
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ProfileDrawer()
+      // AppBar()
+      body: Column(
+        children: [
+          // header()
+          // countInfo()
+          // Buttons()
+          // Tab()
+        ],
+      ),
+    );
+  }
+}
+
