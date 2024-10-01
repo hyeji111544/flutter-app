78c75c9046204cb6cc2c6162fc4cb187640c19de
mouse <hyeji111544@gmail.com>
2024-10-01 16:51:07 +0900

    탭바 완료

--- profileapp/lib/components/profile_tab.dart
+++ profileapp/lib/components/profile_tab.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+
 class ProfileTab extends StatefulWidget {
   @override
-  State<ProfileTab> createState() => _ProfileTabState();
+  _ProfileTabState createState()  => _ProfileTabState();
 }
 
-class _ProfileTabState extends State<ProfileTab> {
+class _ProfileTabState extends State<ProfileTab> with SingleTickerProviderStateMixin{
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         _buildTabBar(),
-        _buildTabView(),
+        Expanded(child: _buildTabBarView()),
+      ],
+    );
+  }
+
+  Widget _buildTabBar(){
+    return TabBar(
+      controller: _tabController,
+      tabs: [
+        Tab(icon: Icon(Icons.directions_car)),
+        Tab(icon: Icon(Icons.directions_transit)),
+      ],
+    );
+  }
+
+  Widget _buildTabBarView(){
+    return TabBarView(
+      controller: _tabController,
+      children: [
+        Container(color: Colors.green),
+        Container(color: Colors.red),
       ],
     );
   }
 }
 
-Widget _buildTabBar(){return SizedBox();}
-Widget _buildTabView(){return SizedBox();}
