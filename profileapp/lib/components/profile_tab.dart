24e0a09f63fb2038a1783adfcb7fdba1c937e667
mouse <hyeji111544@gmail.com>
2024-10-02 10:43:27 +0900

    flutter code 참고해서 스크롤 구현

--- profileapp/lib/components/profile_tab.dart
+++ profileapp/lib/components/profile_tab.dart
@@ -1,69 +1,157 @@
 import 'package:flutter/material.dart';
+void main() => runApp(const MyApp());
 
-class ProfileTab extends StatefulWidget {
-  @override
-  _ProfileTabState createState()  => _ProfileTabState();
-}
-
-class _ProfileTabState extends State<ProfileTab> with SingleTickerProviderStateMixin{
-  TabController? _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = new TabController(length: 2, vsync: this);
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _buildTabBar(),
-        Expanded(child: _buildTabBarView()),
-      ],
-    );
-  }
-
-  Widget _buildTabBar(){
-    return TabBar(
-      controller: _tabController,
-      tabs: [
-        Tab(icon: Icon(Icons.directions_car)),
-        Tab(icon: Icon(Icons.directions_transit)),
-      ],
-    );
-  }
-
-  Widget _buildTabBarView(){
-    return TabBarView(
-      controller: _tabController,
-      children: [
-        GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisSpacing: 10,
-                crossAxisCount: 3,
-                mainAxisSpacing: 10,
-            ),
-            itemCount: 42,
-            itemBuilder: (context, index) {
-              return Image.network(
-                "https://picsum.photos/id/${index + 10}/200/200");
-            },
-        ),
-        GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisSpacing: 10,
-            crossAxisCount: 3,
-            mainAxisSpacing: 10,
-          ),
-          itemCount: 42,
-          itemBuilder: (context, index) {
-            return Image.network(
-                "https://picsum.photos/id/${index + 30}/200/200");
-          },
-        ),
-      ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const ProfilePage(),
     );
   }
 }
 
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: _menu(),
+      appBar: _appbar(),
+      body: ListView(
+        children: [
+          _header(),
+          _middle(),
+          SizedBox(child: _bottom(), height: 3050),
+        ],
+      ),
+    );
+  }
+
+  Row _middle() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Column(
+          children: [
+            Text("50"),
+            Text("10"),
+          ],
+        ),
+        Container(
+          width: 1,
+          height: 100,
+          color: Colors.black,
+        ),
+        Column(
+          children: [
+            Text("50"),
+            Text("10"),
+          ],
+        ),
+        Container(
+          width: 1,
+          height: 100,
+          color: Colors.black,
+        ),
+        Column(
+          children: [
+            Text("50"),
+            Text("10"),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Container _menu() {
+    return Container(
+      width: 200,
+      color: Colors.blue,
+    );
+  }
+
+  AppBar _appbar() {
+    return AppBar(
+      iconTheme: IconThemeData(color: Colors.blue),
+      title: Text("Profile"),
+      centerTitle: true,
+      leading: Icon(Icons.arrow_back_ios),
+    );
+  }
+
+  Widget _header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 80,
+            height: 80,
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://cdn.imweb.me/thumbnail/20240220/105cc1508cf03.png",
+              ),
+            ),
+          ),
+          SizedBox(width: 50),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("A"),
+              Text("B"),
+              Text("C"),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _bottom extends StatelessWidget {
+  const _bottom({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.accessibility_new_outlined)),
+              Tab(icon: Icon(Icons.account_circle_sharp)),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              children: [
+                GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 5,
+                    mainAxisSpacing: 5,
+                  ),
+                  itemBuilder: (context, index) => Image.network(
+                    "https://picsum.photos/id/${200 + index}/200/300",
+                    fit: BoxFit.cover,
+                  ),
+                  itemCount: 30,
+                ),
+                Container(color: Colors.red),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- profileapp/lib/main.dart
+++ profileapp/lib/main.dart
@@ -1,59 +1,164 @@
 import 'package:flutter/material.dart';
-import 'package:profileapp/components/profile_buttons.dart';
-import 'package:profileapp/components/profile_count_info.dart';
-import 'package:profileapp/components/profile_drawer.dart';
-import 'package:profileapp/components/profile_header.dart';
-import 'package:profileapp/components/profile_tab.dart';
-import 'package:profileapp/theme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key : key);
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: theme(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
       home: ProfilePage(),
     );
   }
 }
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key : key);
+  ProfilePage({super.key});
+
+  int size = 30;
 
   @override
   Widget build(BuildContext context) {
+    double bottomHeight = ((size ~/ 3) + (size / 3)) * 205;
     return Scaffold(
-      endDrawer: ProfileDrawer(),// ProfileDrawer()
-      appBar: _buildProfileAppBar(),
-      body: Column(
+      endDrawer: _menu(),
+      appBar: _appbar(),
+      body: ListView(
         children: [
-          // header()
-          const SizedBox(height: 20),
-          ProfileHeader(),
-          // countInfo()
-          const SizedBox(height: 20),
-          ProfileCountInfo(),
-          // Buttons()
-          const SizedBox(height: 20),
-          ProfileButtons(),
-          // Tab()
-          Expanded(child: ProfileTab()),
+          _header(),
+          _middle(),
+          SizedBox(child: _bottom(size), height: bottomHeight),
+        ],
+      ),
+    );
+  }
+
+  Row _middle() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Column(
+          children: [
+            Text("50"),
+            Text("10"),
+          ],
+        ),
+        Container(
+          width: 1,
+          height: 100,
+          color: Colors.black,
+        ),
+        Column(
+          children: [
+            Text("50"),
+            Text("10"),
+          ],
+        ),
+        Container(
+          width: 1,
+          height: 100,
+          color: Colors.black,
+        ),
+        Column(
+          children: [
+            Text("50"),
+            Text("10"),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Container _menu() {
+    return Container(
+      width: 200,
+      color: Colors.blue,
+    );
+  }
+
+  AppBar _appbar() {
+    return AppBar(
+      iconTheme: IconThemeData(color: Colors.blue),
+      title: Text("Profile"),
+      centerTitle: true,
+      leading: Icon(Icons.arrow_back_ios),
+    );
+  }
+
+  Widget _header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 80,
+            height: 80,
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://cdn.imweb.me/thumbnail/20240220/105cc1508cf03.png",
+              ),
+            ),
+          ),
+          SizedBox(width: 50),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("A"),
+              Text("B"),
+              Text("C"),
+            ],
+          )
         ],
       ),
     );
   }
 }
 
-AppBar _buildProfileAppBar(){
-  return AppBar(
-    leading: Icon(Icons.arrow_back_ios),
-    title: Text("Prifile"),
-    centerTitle: true,
-  );
-}
+class _bottom extends StatelessWidget {
+  int size;
+  _bottom(this.size);
 
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.accessibility_new_outlined)),
+              Tab(icon: Icon(Icons.account_circle_sharp)),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              children: [
+                GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 5,
+                    mainAxisSpacing: 5,
+                  ),
+                  itemBuilder: (context, index) => SizedBox(
+                    height: 200,
+                    child: Image.network(
+                      "https://picsum.photos/id/${200 + index}/200/300",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  itemCount: size,
+                  physics: NeverScrollableScrollPhysics(),
+                ),
+                Container(color: Colors.red),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
