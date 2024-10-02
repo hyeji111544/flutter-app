e4adc1c3881a4304efa44ad9ad26e5624ca73765
mouse <hyeji111544@gmail.com>
2024-10-02 11:21:32 +0900

    로그인 폼 생성

--- loginapp/lib/main.dart
+++ loginapp/lib/main.dart
@@ -10,11 +10,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/login",
-      routes: {
-        "/login": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-      }
+        debugShowCheckedModeBanner: false,
+        initialRoute: "/login", // 첫번째 표시될 페이지
+        routes: {
+          "/login": (context) => LoginPage(),
+          "/home": (context) => HomePage(),
+        });
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: "Username",
+                prefixIcon: Icon(Icons.person),
+            ),
+          ),
+          //TextFormFilde 는 InputDecoration 을 사용
+          TextFormField(
+            obscureText: true, //비밀번호 기재시 안보이게 해주는것
+            decoration: InputDecoration(
+                hintText: "Password",
+              prefixIcon: Icon(Icons.password),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                print("버튼 클릭됨");
+              },
+              child: Text("로그인")),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("HomePage")),
     );
   }
 }
