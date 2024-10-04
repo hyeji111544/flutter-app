e0288f3767a17eef020b8fe7b0b0ae65fd3d3bfa
hyeji111544 <154953972+hyeji111544@users.noreply.github.com>
2024-10-04 15:03:40 +0900

    홈 화면 완료

--- marketapp/lib/screens/components/product_item.dart
+++ marketapp/lib/screens/components/product_item.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:marketapp/models/product.dart';
+import 'package:marketapp/screens/components/product_detail.dart';
 
+class ProductItem extends StatelessWidget {
+  final Product product;
+  ProductItem({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 135.0,
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.network(
+              product.urlToImage,
+              width: 115,
+              height: 115,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 16.0),
+          ProductDetail(product: product)
+        ],
+      ),
+    );
+  }
+}
