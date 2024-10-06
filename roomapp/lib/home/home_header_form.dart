4131dd115f28deb7a732450d1cd45f2ae6a02165
hyeji111544 <hyeji111544@gmail.com>
2024-10-06 22:53:33 +0900

    검색필드 완성

--- roomapp/lib/home/home_header_form.dart
+++ roomapp/lib/home/home_header_form.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:roomapp/common/common_form_field.dart';
+import 'package:roomapp/constants.dart';
 import 'package:roomapp/size.dart';
+import 'package:roomapp/styles.dart';
 
 class HomeHeaderForm extends StatelessWidget {
   @override
@@ -30,18 +33,92 @@ class HomeHeaderForm extends StatelessWidget {
       ),
     );
   }
+
   Widget _buildFormSubmit() {
-    return SizedBox();
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: kAccentColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            )),
+        onPressed: () {
+          print("서브밋 클릭");
+        },
+        child: Text(
+          "검색",
+          style: subtitle1(mColor: Colors.white),
+        ),
+      ),
+    );
   }
+
   Widget _buildFormField() {
-    return SizedBox();
+    return Column(
+      children: [
+        CommonFormField(
+          prefixText: "위치",
+          hintText: "근처 추천 장소",
+        ),
+        SizedBox(
+          height: gap_s,
+        ),
+        Row(
+          children: [
+            Expanded(
+                child: CommonFormField(
+              prefixText: "체크인",
+              hintText: "날짜 입력",
+            )),
+            Expanded(
+                child: CommonFormField(
+              prefixText: "체크 아웃",
+              hintText: "날짜 입력",
+            )),
+          ],
+        ),
+        SizedBox(height: gap_s),
+        Row(
+          children: [
+            Expanded(
+                child: CommonFormField(
+              prefixText: "성인",
+              hintText: "2",
+            )),
+            Expanded(
+                child: CommonFormField(
+              prefixText: "어린이",
+              hintText: "2",
+            )),
+          ],
+        ),
+        SizedBox(
+          height: gap_m,
+        ),
+      ],
+    );
   }
 
   Widget _buildFormTitle() {
-    return SizedBox();
+    return Column(
+      children: [
+        Text(
+          "모두의숙소에서 숙소를 검색하세요.",
+          style: h4(),
+        ),
+        SizedBox(
+          height: gap_xs,
+        ),
+        Text(
+          "혼자하는 여행에 적합한 개인실부터 여럿이 함께하는 여행에 좋은 '공간전체' 숙소까지, 모두의 숙소에 다 있습니다.",
+          style: body1(),
+        ),
+        SizedBox(
+          height: gap_m,
+        ),
+      ],
+    );
   }
 }
-
-
-
-
